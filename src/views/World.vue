35b39c2824ea7c24d44f18961c0a117ea4d30344
knowmore <goooog@88.com>
2021-04-06 12:51:22 +0800

    fix leaflet icon

--- src/views/World.vue
+++ src/views/World.vue
@@ -16,6 +16,14 @@ import { GridComponent, LegendComponent } from 'echarts/components';
 import { BarChart } from 'echarts/charts';
 import { CanvasRenderer } from 'echarts/renderers';
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 const worldData = require("../static/resource/data.json");
 const countriesName = Object.keys(worldData.countries);
 

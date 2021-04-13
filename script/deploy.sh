109a845108af4bd3666f25b2a2156b0c20702772
knowmore <goooog@88.com>
2021-04-13 19:57:17 +0800

    update deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,3 +4,4 @@ curl https://disease.sh/v3/covid-19/countries -O src/static/resource/countries.j
 npm run build
 trash-put ./_deploy/*
 cp -r ./dist/* ./_deploy
+cp -r ./src/static/resource ./_deploy/data
